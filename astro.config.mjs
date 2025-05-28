0d76bbf4e9c683f4dc29b5939dc78fe3d8b11063
David Wahrenburg <mail@davidwahrenburg.de>
2025-05-28 15:33:50 +0200

    setup gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+  site: 'https://schnavy.github.io',
+  base: '/astro-base', // Only needed if deploying to a project repo
+  output: 'static'
+});
